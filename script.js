40c9452313921f6a10295c3b5665c47d2766fd05
Dr Lawless Lawyer <163570797+Mr-MaF@users.noreply.github.com>
2025-04-25 00:16:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-document.getElementById('themeToggle').onclick = function() {
-    document.body.classList.toggle('dark-theme');
-    localStorage.setItem('theme', document.body.classList.contains('dark-theme') ? 'dark' : 'light');
-};
-
 window.onload = function() {
     const savedTheme = localStorage.getItem('theme');
     if (savedTheme === 'dark') {
         document.body.classList.add('dark-theme');
     }
+};
+
+document.getElementById('themeToggle').onclick = function() {
+    document.body.classList.toggle('dark-theme');
+    localStorage.setItem('theme', document.body.classList.contains('dark-theme') ? 'dark' : 'light');
 };
